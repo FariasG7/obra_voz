0fba2d136bceff5e4c2fb7de02e1c764e99e3e1f
FariasG7 <farias.g7@icloud.com>
2026-08-27 22:12:13 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ export default defineConfig({
   plugins: [react()],
   // Se o seu repositório for "diario-obra", coloque o nome dele entre as barras:
   // Se não tiver certeza, use './' que funciona na maioria dos casos
-  base: '/codespaces-react/', 
+  base: '/', 
 })
